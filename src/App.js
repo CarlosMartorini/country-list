2933e2f409a139884357239fbd3b2fe5b5b0f044
Carlos Martorini <c7.silveira@gmail.com>
2021-08-24 01:34:51 -0300

    style: Created styles from home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
 import HeaderBar from './components/HeaderBar';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HeaderBar/>
-        <Home/>
-      </header>
-    </div>
+    <>
+      <HeaderBar/>
+      <Home/>
+    </>
   );
 }
 
